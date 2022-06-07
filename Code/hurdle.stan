80b8c9a5450224264fcbf754c8d6b5c42ed21652
jun-jieh <70912336+jun-jieh@users.noreply.github.com>
2022-06-07 16:36:11 -0400

    Add files via upload

---
+++ Code/hurdle.stan
@@ -0,0 +1,156 @@
+// Input data
+data {
+  int<lower=0> N;      //number of observations
+  int<lower=0> Nanim;  //number of animals (giver/receiver)
+  int<lower=0> Ngroup; //number of social groups
+  real y[N];           //duration of attacking time
+  int receiver_id[N];  //receiver id from 1...Nanim
+  int giver_id[N];     //giver id from 1...Nanim
+  int group_id[N];     //social group id from 1...Ngroup
+  vector[N] weight_receiver; //(centered) weight of receiver
+  vector[N] weight_giver;    //(centered) weight of giver
+  vector[N] sex;             //sex: 1= barrow
+  vector[N] nursery;         //nursery mate status: 1=shared nursery pen
+  vector[N] litter;          //litter mate status: 1=littermates
+  
+}
+
+// The (co)variance matrix for the random giver effect and random receiver effect
+transformed data{
+  vector[2] mu;
+  cov_matrix[2] S = [[5, 0], [0, 5]];
+  
+  for(i in 1:2) mu[i]=0; 
+  
+}
+
+// The parameters accepted by the model.
+parameters {
+  real alpha;                 //intercept, lognormal
+  real sex_eff;               //sex effect, lognormal
+  real nursery_eff;           // common nursery effect, lognormal
+  real litter_eff;            // common litter effect, lognormal
+  real weight_receiver_eff;   //slope of receiver weight, lognormal
+  real weight_giver_eff;      // slope of giver weight, lognormal
+  
+  vector[Ngroup] group_eff;   //group random effect, lognormal
+  
+  matrix[Nanim,2] Animal;     // matrix for the two animals, lognormal
+  cov_matrix[2] Sigma;
+  
+  real<lower=0> sigma_e;      //residual standard deviation, lognormal
+  real<lower=0> sigma_group;  //group standard deviation, lognormal
+
+  real alpha0;                //intercept, Bernoulli
+  real sex_eff0;              //sex effect, Bernoulli
+  real nursery_eff0;          // common nursery effect, Bernoulli
+  real litter_eff0;           // common litter effect, Bernoulli
+  real weight_receiver_eff0;  //slope of receiver weight, Bernoulli
+  real weight_giver_eff0;     // slope of giver weight, Bernoulli
+  
+  vector[Ngroup] group_eff0;  //group random effect, Bernoulli
+  
+  matrix[Nanim,2] Animal0;    // matrix for the two animals, Bernoulli
+  cov_matrix[2] Sigma0;
+
+  real<lower=0> sigma_group0; //group standard deviation, Bernoulli
+
+}
+
+transformed parameters {
+  vector[N] eta;   //for each observation compute the linear predictor, lognormal
+  vector[N] eta0;  //for each observation compute the linear predictor, Bernoulli
+  real<lower=0, upper=1> theta[N]; //probability of being 0
+
+  for (i in 1:N)
+  eta[i]=alpha+sex[i]*sex_eff+litter[i]*litter_eff+nursery[i]*nursery_eff+weight_receiver[i]*weight_receiver_eff+weight_giver[i]*weight_giver_eff+group_eff[group_id[i]]+Animal[giver_id[i],1]+Animal[receiver_id[i],2];
+
+  for (i in 1:N)
+  eta0[i]=alpha0+sex[i]*sex_eff0+litter[i]*litter_eff0+nursery[i]*nursery_eff0+weight_receiver[i]*weight_receiver_eff0+weight_giver[i]*weight_giver_eff0+group_eff0[group_id[i]]+Animal0[giver_id[i],1]+Animal0[receiver_id[i],2];
+
+  for (i in 1:N)
+  theta[i] = inv_logit(eta0[i]);
+
+}
+
+model {
+  Sigma ~ wishart(4, S);  
+  Sigma0 ~ wishart(4, S);  
+
+  
+  sigma_group~uniform(0,10);                   //prior random eff
+  sigma_e~uniform(0,100);                      //prior random eff
+
+  group_eff~normal(0,sigma_group);             //prior random eff
+  
+  sex_eff~uniform(-10,10);                     //prior fixed eff
+  litter_eff~uniform(-10,10);                  //prior fixed eff
+  nursery_eff~uniform(-10,10);                 //prior fixed eff
+  alpha~uniform(-10,10);                       //prior fixed eff
+  
+  weight_receiver_eff~uniform(-100,100);       //prior fixed eff
+  weight_giver_eff~uniform(-100,100);          //prior fixed eff
+
+  sigma_group0~uniform(0,10);                  //prior random eff
+  group_eff0~normal(0,sigma_group0);           //prior random eff
+  
+  sex_eff0~uniform(-10,10);                    //prior fixed eff
+  litter_eff0~uniform(-10,10);                 //prior fixed eff
+  nursery_eff0~uniform(-10,10);                //prior fixed eff
+  alpha~uniform(-10,10);                       //prior fixed eff
+  
+  weight_receiver_eff0~uniform(-100,100);       //prior fixed eff
+  weight_giver_eff0~uniform(-100,100);          //prior fixed eff
+
+  
+  for (i in 1:Nanim)
+      Animal[i]~multi_normal(mu,Sigma);
+  for (i in 1:Nanim)
+      Animal0[i]~multi_normal(mu,Sigma0);
+  
+  
+  // Likelihood
+  for (i in 1:N) {
+    if (y[i] == 0) {
+    target += log(theta[i]);
+    } else {
+      target += log1m(theta[i]) + lognormal_lpdf(y[i] | eta[i],sigma_e);
+    }
+  }
+
+
+}
+
+
+generated quantities{
+  real <lower=0> sigma_receiver;
+  real <lower=0> sigma_receiver0;
+  real <lower=0> sigma_giver;
+  real <lower=0> sigma_giver0;
+  real <lower=-1, upper=1> cor_g_r;
+  real <lower=-1, upper=1> cor_g_r0;
+  real weight_diff;
+  real weight_diff0;
+  real y_rep[N];
+  int struc_zero;
+  
+  sigma_receiver = Sigma[2,2];
+  sigma_receiver0 = Sigma0[2,2];
+  sigma_giver = Sigma[1,1];
+  sigma_giver0 = Sigma0[1,1];
+  cor_g_r = Sigma[1,2]/sqrt(sigma_giver*sigma_receiver);
+  cor_g_r0 = Sigma0[1,2]/sqrt(sigma_giver0*sigma_receiver0);
+  weight_diff = weight_giver_eff-weight_receiver_eff;
+  weight_diff0 = weight_giver_eff0-weight_receiver_eff0;
+  
+  for (i in 1:N){
+   struc_zero = bernoulli_rng(theta[i]);
+   if (struc_zero == 1) {
+     y_rep[i]=0.0;
+   } else {
+     y_rep[i]=lognormal_rng(eta[i],sigma_e);
+   }
+
+  }
+  
+}
---
+++ Code/runBayes.R
@@ -0,0 +1,130 @@
+# Load all the required packages for the implementation
+# Note: if the packages are not installed in the local computer, use install.packages() to install them
+library(ggplot2)
+library(bayesplot)
+library(rstan)
+
+# For execution on a local, multicore CPU with excess RAM the following two lines are recommended: 
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+# Load the data file that is also provided in this GitHub repository
+load("~/dyad_data.RData")
+# Set working path (optional)
+# setwd("~/Dyadic data analysis")
+
+# Create a dataset for the stan program
+# Response variable
+y<-dyad_data$y
+# Indicator of the previous litter mate experience 
+litter<-as.numeric(dyad_data$litter)-1
+# Sex of the dyad (pigs were housed in single-sex pens)
+sex<-1*(dyad_data$sex=="b")
+# Indicator of the previous nursery mate experience
+nursery<-as.numeric(dyad_data$nursery)-1
+# Social group index
+social_group<-as.numeric(droplevels(dyad_data$social_group))
+# Aggression giver ID
+giver_id<-as.numeric(droplevels(dyad_data$giver_id))
+# Aggression receiver ID
+receiver_id<-as.numeric(droplevels(dyad_data$receiver_id))
+
+
+# Put data into a list. Note: c_weight means the centered weight within social groups
+data<-list(N=length(y),
+           Nanim=length(unique(receiver_id)),
+           Ngroup=length(unique(social_group)),
+           y=y,
+           receiver_id=receiver_id,
+           giver_id=giver_id,
+           group_id=social_group,
+           weight_receiver=dyad_data$c_weight_receiver,
+           weight_giver=dyad_data$c_weight_giver,
+           sex=sex,
+           nursery=nursery,
+           litter=litter)
+
+# Time profiler (start recording time)
+strt_time <- Sys.time()
+
+# Call the stan code generator, compile, and execute the model fitting
+hurdle_model  <-  stan(file = "hurdle.stan",data = data,
+                                      chains = 4,warmup = 20,iter = 50,thin = 10,seed=22222,
+                                      control = list(max_treedepth = 12),
+                                      pars = c("alpha",
+                                               "sex_eff",
+                                               "nursery_eff",
+                                               "litter_eff",
+                                               "weight_receiver_eff",
+                                               "weight_giver_eff",
+                                               "Sigma",
+                                               "sigma_e",
+                                               "sigma_group",
+                                               "alpha0",
+                                               "sex_eff0",
+                                               "nursery_eff0",
+                                               "litter_eff0",
+                                               "weight_receiver_eff0",
+                                               "weight_giver_eff0",
+                                               "Sigma0",
+                                               "sigma_group0",
+                                               "sigma_receiver",
+                                               "sigma_receiver0",
+                                               "sigma_giver",
+                                               "sigma_giver0",
+                                               "cor_g_r",
+                                               "cor_g_r0",
+                                               "weight_diff",
+                                               "weight_diff0",
+                                               "y_rep"
+                                               ))
+
+# Save the 
+save(hurdle_model, file="DyadAnalysis_output.RData")
+
+
+# Time profiler (compute the time elapsed)
+end_time <- Sys.time()
+runtime <- end_time - strt_time
+print(runtime)
+
+# Convergence diagnostics
+check_hmc_diagnostics(hurdle_model)
+
+# We have so many variables. So, we will generate multiple trace plots of the MCMC chains
+# Note: the variable names are explained in the stan code
+mcmc_trace(hurdle_model,pars=c("alpha", "sex_eff","nursery_eff","litter_eff","weight_receiver_eff","weight_giver_eff"))
+
+mcmc_trace(hurdle_model,pars=c("sigma_e",
+                               "Sigma[1,1]",
+                               "Sigma[1,2]",
+                               "Sigma[2,2]",
+                               "sigma_group"))
+
+mcmc_trace(hurdle_model,pars=c("alpha0", "sex_eff0","nursery_eff0","litter_eff0","weight_receiver_eff0","weight_giver_eff0"))
+
+mcmc_trace(hurdle_model,pars=c("Sigma0[1,1]",
+                               "Sigma0[1,2]",
+                               "Sigma0[2,2]",
+                               "sigma_group0"))
+
+
+# We have so many variables. So, we will generate multiple autocorrelation plots
+# Note: the variable names are explained in the stan code
+mcmc_acf(hurdle_model,pars=c("alpha", "sex_eff","nursery_eff","litter_eff","weight_receiver_eff","weight_giver_eff"))
+
+mcmc_acf(hurdle_model,pars=c("sigma_e",
+                             "Sigma[1,1]",
+                             "Sigma[1,2]",
+                             "Sigma[2,2]",
+                             "sigma_group"))
+
+mcmc_acf(hurdle_model,pars=c("alpha0", "sex_eff0","nursery_eff0","litter_eff0","weight_receiver_eff0","weight_giver_eff0"))
+
+mcmc_acf(hurdle_model,pars=c("Sigma0[1,1]",
+                             "Sigma0[1,2]",
+                             "Sigma0[2,2]",
+                             "sigma_group0"))
+
+# Finally, obtain the posterior distribution of the estimated parameters for Bayesian inference
+round(summary(hurdle_model)$summary,3)
